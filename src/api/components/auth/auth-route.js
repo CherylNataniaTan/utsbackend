3488ec34a46ba90ad24dd47e86f937b1c7c8cb33
marcellokwandinata <marcellokwandinata@gmail.com>
2026-04-23 18:57:30 +0700

    menambahkan auth-route

---
+++ src/api/components/auth/auth-route.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const authController = require('./auth-controller');
+const { authMiddleware } = require('../../middlewares');
+
+const route = express.Router();
+
+module.exports = (app) => {
+  app.use('/auth', route);
+
+  route.get('/', authMiddleware, authController.getAuthSessions);
+
+  route.get('/protected', authMiddleware, authController.testProtected);
+
+  route.get('/:id', authMiddleware, authController.getAuthSession);
+
+  route.post('/login', authController.login);
+
+  route.patch('/:id', authMiddleware, authController.updateAuthSession);
+
+  route.delete('/:id', authMiddleware, authController.deleteAuthSession);
+};
