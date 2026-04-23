d4b8c37d0a2256a80b4a0f8306b74bc493a52420
WilbertAllanMuljadi <wilbert.535250053@stu.untar.ac.id>
2026-04-23 11:45:36 +0700

    repository acc

---
+++ src/api/components/accounts/accounts-repository.js
@@ -0,0 +1,37 @@
+console.log("🔥 REPOSITORY BARU KELOAD 🔥");
+const { Accounts } = require('../../../models');
+
+async function findAll() {
+  return await Accounts.find().sort({ createdAt: -1 });
+}
+
+async function findByAccountNumber(accountNumber) {
+  return await Accounts.findOne({ accountNumber: String(accountNumber) });
+}
+
+async function create(data) {
+  return await Accounts.create(data);
+}
+
+
+async function updateByAccountNumber(accountNumber, data) {
+  return await Accounts.findOneAndUpdate(
+    { accountNumber: String(accountNumber) },
+    data,
+    { new: true }
+  );
+}
+
+async function deleteByAccountNumber(accountNumber) {
+  return await Accounts.findOneAndDelete({
+    accountNumber: String(accountNumber),
+  });
+}
+
+module.exports = {
+  findAll,
+  findByAccountNumber,
+  create,
+  updateByAccountNumber,
+  deleteByAccountNumber,
+};
