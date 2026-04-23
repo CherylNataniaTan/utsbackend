eb10b666f6dabd7c472d1ce58b006181c6243b12
frengky <frengkydoang01@gmail.com>
2026-04-23 14:50:15 +0700

    uts frengky aman trus

---
+++ src/api/components/transfers/transfer-repository.js
@@ -0,0 +1,34 @@
+const Transfer = require('../../../models/transfers-schema');
+
+// ambil semua data transfer
+const getAllTransfers = async () => {
+  return await Transfer.find();
+};
+
+// ambil transfer by id
+const getTransferById = async (id) => {
+  return await Transfer.findById(id);
+};
+
+// buat transfer baru
+const createTransfer = async (data) => {
+  return await Transfer.create(data);
+};
+
+// update transfer
+const updateTransfer = async (id, data) => {
+  return await Transfer.findByIdAndUpdate(id, data, { new: true });
+};
+
+// hapus transfer
+const deleteTransfer = async (id) => {
+  return await Transfer.findByIdAndDelete(id);
+};
+
+module.exports = {
+  getAllTransfers,
+  getTransferById,
+  createTransfer,
+  updateTransfer,
+  deleteTransfer
+};
